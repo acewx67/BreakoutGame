47f8de06401474cf5014a3232857a4fbb1d3179f
Ace <mohammed.ashfaq1127@gmail.com>
2022-12-01 12:50:00 +0530

    First commit

---
+++ src/game/Commons.java
@@ -0,0 +1,14 @@
+package game;
+
+public interface Commons {
+
+    int WIDTH = 300;   //width of window
+    int HEIGHT = 400;   //height
+    int BOTTOM_EDGE = 390;   //ball is out
+    int N_OF_BRICKS = 30;   //no. of bricks
+    int INIT_PADDLE_X = 200;   // x and y give position of paddle/platform
+    int INT_PADDLE_y = 360;
+    int INIT_BALL_X = 230;  //x and y give position of ball
+    int INIT_BALL_Y = 355;
+    int PERIOD = 10; //speed
+}
